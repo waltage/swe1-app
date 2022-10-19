81c2bcf8c09fc1a9ec171682983b8485cab2f0a6
Daniel Walt <daniel.r.walt@gmail.com>
2022-10-19 13:03:31 -0400

    one more lint change?

--- shortlist/backend/polls/models.py
+++ shortlist/backend/polls/models.py
@@ -13,7 +13,9 @@ class Question(models.Model):
     def __str__(self):
         return "Q{:02d}-{}".format(self.id, self.question_text)
 
-    @admin.display(boolean=True, ordering="pub_date", description="Published Recently?")  # noqa: E501
+    @admin.display(
+        boolean=True, ordering="pub_date", description="Published Recently?"
+    )  # noqa: E501
     def was_published_recently(self):
         upper = timezone.now()
         lower = upper - dt.timedelta(days=1)
