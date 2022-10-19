09aa9dfd416ebcf46dd149c80983fdee1917ea7b
Daniel Walt <daniel.r.walt@gmail.com>
2022-10-19 12:08:06 -0400

    add coveraalls

--- ci/testing/pytest_django.py
+++ ci/testing/pytest_django.py
@@ -2,20 +2,33 @@ import argparse
 import coverage
 import django
 import os
+import sys
+import json
+import requests
+import subprocess
 
 from django.conf import settings
 from django.test.utils import get_runner
 
+import hashlib
+import datetime as dt
+
 PARSER = argparse.ArgumentParser(description="Test with Coverage")
 PARSER.add_argument("--output_dir", type=str, required=True)
 
-
 if __name__ == "__main__":
+  print(os.getcwd())
   args = PARSER.parse_args()
   cov_dir = os.path.join(args.output_dir, "coverage")
-  data_file = os.path.join(cov_dir, ".db")
+  data_file = os.path.join(cov_dir, ".coverage")
+  lcov_file = os.path.join(cov_dir, "coverage.lcov")
 
-  cov = coverage.Coverage(data_file=data_file)
+  cov = coverage.Coverage(
+    data_file=data_file,
+    source_pkgs=[
+      "shortlist",
+      ],
+    )
   cov.start()
 
   os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shortlist.backend.hello.settings")
@@ -23,7 +36,7 @@ if __name__ == "__main__":
   TestRunner = get_runner(settings)
   test_runner = TestRunner()
 
-  test_runner.run_tests(
+  fails = test_runner.run_tests(
     [ 
       "shortlist.backend.hello",
       "shortlist.backend.polls",
@@ -32,4 +45,6 @@ if __name__ == "__main__":
 
   cov.stop()
   cov.save()
-  print("covered: ", cov.html_report(directory=cov_dir))
+  cov.lcov_report(outfile=lcov_file)
+
+  sys.exit(fails)
