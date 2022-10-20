f47a687d2fcbe17e10a0c43d03e4c74bcedd338b
Daniel Walt <daniel.r.walt@gmail.com>
2022-10-20 02:07:47 +0000

    add deploy to travis

--- ci/deploy/deploy.sh
+++ ci/deploy/deploy.sh
@@ -13,9 +13,11 @@ docker build . \
 
 docker push .
 
-# set 0
-aws ecs update-service --cluster $AWS_CLUSTER --service $AWS_SERVICE --desired-count 0 --no-paginate >/dev/null
 
+# set 0
+echo "Reducing ECS capacity to 0..."
+aws ecs update-service --cluster $AWS_CLUSTER --service $AWS_SERVICE --desired-count 0 --no-paginate >/dev/null
+echo "    updated."
 # stop current
 tasks=$(aws ecs list-tasks --cluster $AWS_CLUSTER --service $AWS_SERVICE | grep -o -P '(arn.*[^"])')
 echo $alltasks
@@ -23,5 +25,17 @@ while IFS= read -r task; do
   echo "Stopping: $task"
   aws ecs stop-task --cluster $AWS_CLUSTER --task $task >/dev/null
 done <<< "$tasks"
+echo "Stopped all tasks.  Waiting for new deployment."
+sleep 5;
+echo "Forcing new deployment..."
 # force 1
 aws ecs update-service --cluster $AWS_CLUSTER --service $AWS_SERVICE --desired-count 1 --force-new-deployment >/dev/null
+
+DESIRED_ENDPOINT=http://lb-django-hello-516063654.us-east-1.elb.amazonaws.com/polls/
+
+while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' $DESIRED_ENDPOINT)" != "200" ]]; do 
+  sleep 5
+  echo "... waiting for 200"
+done
+
+echo "Deployment Completed."
